e255269251df2a2bb39325c582ea0a5518c92d53
CoelhoIN <coelhooisadora@gmail.com>
2025-12-08 16:16:19 -0300

    add force-dynamic

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server"
 import { PrismaClient } from "@prisma/client"
 import { hash } from "bcryptjs"
 
+export const dynamic = "force-dynamic"
+
 const prisma = new PrismaClient()
 
 type Service = {
